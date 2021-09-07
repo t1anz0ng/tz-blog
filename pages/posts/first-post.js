48ac8c4ab9196f8ad87059e7168cd57c780fb48b
TianZong48 <tianzong48@gmail.com>
2021-09-08 00:47:28 +0800

    init

---
+++ pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+
+export default function(){
+    return (
+        <Layout>
+        <Head>
+            <title>First</title>
+        </Head>
+            <h1>First POst</h1>
+            <h2>
+                <Link href="/">
+                    <a>back to home</a>
+                </Link>
+            </h2>
+        </Layout>
+    )
+}
